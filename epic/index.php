1660cfae5ed889d6277faf2d60603b96dde2da38
Daniel Gonzales <dgonzales371@cnm.edu>
2020-01-14 10:31:52 -0700

    Data Design project phase 1

---
+++ epic/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Title</title>
+</head>
+<body>
+<h1>Data Design Project</h1>
+<a href="persona.php"> This is my persona project!</a>
+<a href="user-story.php"> I will be the using Dungeons and Dragons as the base for my project</a>
+<a href="user-case-interaction.php"> It is time to initiate a good user experiance</a>
+
+</body>
+</html>
